6ecbc6a49f286f2abca8992ae3d24aa277f21ce2
YUAN Zijing <Zijing.YUAN@murex.com>
2018-01-10 18:16:45 +0800

    update custom type printer definition

--- test/lib/types_FOO.cpp
+++ test/lib/types_FOO.cpp
@@ -5,8 +5,8 @@
 
 void print_struct_FOO(const mxProc * proc, const char *name, const char *comment, Elf_Addr value)
 {
-	print_int(name, "a", proc, value + offsetof(struct FOO, a));
-	print_int(name, "b", proc, value + offsetof(struct FOO, b));
+	print_int(name, "*a", proc, value + offsetof(struct FOO, a));
+	print_int(name, "*b", proc, value + offsetof(struct FOO, b));
 	print_int(name, "c", proc, value + offsetof(struct FOO, c));
 	print_long(name, "al", proc, value + offsetof(struct FOO, al));
 	print_long(name, "*alp", proc, value + offsetof(struct FOO, alp));
