1b5ad22cf2f22c691fc7b449cc581cf8dd2f4fd0
YUAN Zijing <Zijing.YUAN@murex.com>
2018-01-19 10:00:08 +0800

    remove contributor from source

--- src/pmx.c
+++ src/pmx.c
@@ -6,12 +6,6 @@
 * which accompanies this distribution, and is available at
 * http://www.eclipse.org/legal/epl-v10.html
 *
-* Contributors:
-*    MARTENSON Elias
-*    ANDERSON Philip
-*    LEGRAND Mathieu
-*    MATOT Jerome
-*    YUAN Zijing
 *******************************************************************************/
 
 #include <stdio.h>
